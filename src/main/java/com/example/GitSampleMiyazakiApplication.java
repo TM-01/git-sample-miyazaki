d0bdfb039267fb859ccc8f00154220c5ce402776
TM-01 <tatsuro.miyazaki@rakus.co.jp>
2020-06-16 11:34:19 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/com/example/GitSampleMiyazakiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleMiyazakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleMiyazakiApplication.class, args);
+	}
+
+}
